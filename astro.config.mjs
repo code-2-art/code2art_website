56628c07890966ceae16fc21efed34d4b70909c0
avantcontra <dadadaluo@gmail.com>
2026-02-04 21:58:02 +0800

    add website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://example.com', // 部署后可改
+  site: 'https://code2art.com', // 部署后可改
   integrations: [tailwind({ applyBaseStyles: true })],
   output: 'static'
 });
